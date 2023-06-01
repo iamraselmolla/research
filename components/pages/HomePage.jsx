55b383cc297f0ac1116cec82a0f4b2ab13e5b22f
Aryan Thakur <aryan197297@gmail.com>
2023-06-01 14:10:41 +0530

    add structure of home page

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -41,6 +41,32 @@ const HomePage = () => {
     )
   }
 
+  const CommunityCard = ({ name, designation }) => {
+    return (
+      <div className='bg-blue-500 h-52 w-52 text-right p-4 rounded-md text-white'>
+        <h2 className='text-xl'>{name}</h2>
+        <p>{designation}</p>
+      </div>
+    )
+  }
+
+  const ProductCard = ({ image }) => {
+    return (
+      <div className='h-52 w-40 bg-blue-500 rounded-tr-md rounded-bl-md'>
+        {/* Image here with the link */}
+      </div>
+    )
+  }
+
+  const TestimonialCard = () => {
+    return (
+      <>
+        {/* <img src={Image} alt="Testimonial" /> */}
+        <div className='rounded-md w-56 h-36 bg-green-700'></div>
+      </>
+    )
+  }
+
   return (
     <>
       {loading ? <SplashScreen /> :
@@ -122,6 +148,131 @@ const HomePage = () => {
                 </ul>
 
               </div>
+            </div>
+          </div>
+
+          <div className='bg-white flex flex-col gap-5 py-10'>
+            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>AUTHOR COMMUNITY</h2>
+            <p className='text-black text-center md:w-[50%] self-center'>Become one of the <a className='text-blue-500'>#CLEVERAUTHOR</a> by publishing your Paperback,
+              E-Book & sell globally in 140+ countries, earning 100% of the profit with
+              the matchless help from our experts.</p>
+            <div className='flex w-[96%] md:w-[80%] m-auto overflow-x-hidden items-center'>
+              <button type='button' className='text-black'>{"<"}</button>
+              <div className='flex w-[96%] m-auto overflow-x-hidden items-center'>
+                <div className='inline-flex gap-2 bg-white text-black'>
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                  <CommunityCard name={'John Doe'} designation={'Developer'} />
+                </div>
+              </div>
+              <button type='button' className='text-black'>{">"}</button>
+            </div>
+          </div>
+          <div className='bg-white flex flex-col gap-5 py-10'>
+            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>WHY CLEVER FOX?</h2>
+            <p className='text-black text-center md:w-[50%] self-center'>
+              It is one thing to be an author and it is another thing to be a <a className='text-blue-500'>#CLEVERAUTHOR</a> We have a blind eye towards your experience. At
+              Clever Fox, authors of all stages and genres can avail the following
+              comforts:
+            </p>
+            <div className='flex w-[96%] md:w-[80%] m-auto overflow-x-hidden items-center'>
+              <div className='flex bg-white text-black w-full m-auto'>
+                <div className='bg-blue-500 w-full h-[50vh] rounded-md'></div>
+                <div className='flex flex-col items-center min-w-[50%]'>
+                  <ul className='flex flex-col gap-5 list-disc'>
+                    <li>Highest Royalty rates</li>
+                    <li>Live Royalty Track Dashboard</li>
+                    <li>Expeditious publishing process </li>
+                    <li>Expanded global distribution </li>
+                    <li>Physical store distribution Support </li>
+                    <li>Hand picked publishing team</li>
+                  </ul>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='bg-white flex flex-col gap-5 py-20'>
+            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>PRODUCTS</h2>
+            <p className='text-black text-center md:w-[50%] self-center'>
+              Read the collections of our top trending <b>Self Publishing</b> and <b>Partnered Publishing books</b>
+            </p>
+            <div className='w-[96%] md:w-[80%] m-auto overflow-x-hidden flex flex-col items-center gap-5'>
+              <div className='flex w-[96%] m-auto overflow-x-hidden items-center'>
+                <div className='inline-flex gap-2 bg-white text-black'>
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                </div>
+              </div>
+              <button type='button' className='text-white bg-ter w-32 p-2 rounded-md drop-shadow-sm'>See More</button>
+            </div>
+          </div>
+          <div className='bg-white flex flex-col gap-5 py-20'>
+            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>AUTHOR DASHBOARD</h2>
+            <p className='text-black text-center md:w-[50%] self-center'>
+              Get real-time numbers on your sales and track the order status.
+            </p>
+            <div className='flex bg-white text-black w-[96%] md:w-[80%] m-auto'>
+              <div className='flex flex-col items-center min-w-[50%]'>
+                <ul className='flex flex-col gap-5'>
+                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 border-blue-700 p-2 w-56'>Sales Tracker {">>"}</button>
+                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>Instant Order {">>"}</button>
+                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>Advert Dashboard {">>"}</button>
+                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>Postsales Analysis {">>"}</button>
+                </ul>
+              </div>
+              <div className='bg-blue-500 w-full h-[50vh] rounded-md'></div>
+            </div>
+          </div>
+          <div className='bg-white flex flex-col gap-5 py-20'>
+            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>TESTIMONIALS</h2>
+            <p className='text-black text-center md:w-[50%] self-center'>
+              We are the best at books. But don’t take the word for it. Hear directly
+              from the authors who experienced the transforming publishing journey.
+            </p>
+            <div className='grid place-items-center md:grid-cols-4 grid-cols-1 gap-5 bg-white text-black w-[96%] md:w-[80%] m-auto'>
+              <TestimonialCard />
+              <TestimonialCard />
+              <TestimonialCard />
+              <TestimonialCard />
+            </div>
+            <button className='text-black border-2 border-primary w-40 m-auto rounded-sm p-2 hover:bg-primary'>Read More</button>
+          </div>
+          <div className='bg-white flex flex-col gap-5 py-4'>
+            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>PARTNERS</h2>
+            <p className='text-black text-center md:w-[50%] self-center'>
+              We are the best at books. But don’t take the word for it. Hear directly
+              from the authors who experienced the transforming publishing journey.
+            </p>
+            <div className='grid place-items-center md:grid-cols-8 sm:grid-cols-5 grid-cols-3 gap-5 bg-white text-black w-[96%] md:w-[80%] m-auto'>
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+              <div className='rounded-full bg-green-700 h-24 w-24' />
+            </div>
+          </div>
+          <div className='bg-white flex flex-col gap-5 py-4'>
+            <h2 className='tracking-wider text-2xl text-ter font-bold text-center'>We are a member of the Federation of Indian Publishers</h2>
+            <div className='bg-green-700 w-[60%] h-40 m-auto shadow-xl'>
             </div>
           </div>
 
