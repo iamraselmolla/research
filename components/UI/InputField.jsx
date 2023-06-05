bb4c815116c66f51b0d7d8355807ba6ed7340dbb
Aryan Thakur <aryan197297@gmail.com>
2023-06-05 22:47:37 +0530

    fix: the personal details page

--- components/UI/InputField.jsx
+++ components/UI/InputField.jsx
@@ -30,13 +30,13 @@ const InputField = ({
 
   return (
     <div className={className}>
-      <label className=" text-slate-100  flex items-center" htmlFor={uni}>
+      <label className=" text-black flex items-center" htmlFor={uni}>
         {labelName} {fieldRequired && <span className="text-red-500 ">*</span>}
       </label>
       <div className={`flex items-center space-x-2  rounded-sm  ${disabled ? 'bg-red-100' : 'bg-white'}`}>
         <Field
           disabled={disabled}
-          className={`p-2 w-full text-black rounded-sm ${inputClass} ${disabled ? 'bg-red-100' : 'bg-white'}`}
+          className={`p-2 w-full text-black rounded-lg ${inputClass} ${disabled ? 'bg-red-100' : 'bg-blue-200'}`}
           placeholder={placeholder}
           type={type !== 'password' || type === undefined ? type : type === 'password' && passVisibility ? 'text' : 'password'}
           id={uni}
--- components/user/PersonalDetails.jsx
+++ components/user/PersonalDetails.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Dashboard from '../pages/Dashboard'
 import PersonalDetailsNavigation from '../UI/PersonalDetailsNavigation'
 import { Form, Formik } from 'formik'
-import InputFieldDark from '../UI/InputFieldDark'
+import InputField from '../UI/InputField'
 import * as yup from "yup"
 
 const PersonalDetails = () => {
@@ -11,6 +11,12 @@ const PersonalDetails = () => {
     { placeholder: "Last Name", labelName: "Last Name", uni: "Lastname", initialValue: "", type: "text", fieldrequired: true },
     { placeholder: "DOB", labelName: "DOB", uni: "dob", initialValue: "", type: "text", fieldrequired: true },
     { placeholder: "Gender", labelName: "Gender", uni: "gender", initialValue: "", type: "text", fieldrequired: true },
+    { placeholder: "Nickname", labelName: "Nickname", uni: "nickname", initialValue: "", type: "text", fieldrequired: true },
+    { placeholder: "University Name", labelName: "University Name", uni: "universityName", initialValue: "", type: "text", fieldrequired: true },
+    { placeholder: "Text", labelName: "Course Name", uni: "courseName", initialValue: "", type: "text", fieldrequired: true },
+    { placeholder: "Text", labelName: "Branch", uni: "branch", initialValue: "", type: "text", fieldrequired: true },
+    { placeholder: "Select", labelName: "Year Of Passing", uni: "yop", initialValue: "", type: "text", fieldrequired: true },
+    { placeholder: "Text", labelName: "CGPA", uni: "cgpa", initialValue: "", type: "text", fieldrequired: true },
   ]
 
   const initialValues = {}
@@ -35,7 +41,7 @@ const PersonalDetails = () => {
           <Form className='flex flex-col'>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-5 md:my-5' >
               {fields.map((item, i) => (
-                <InputFieldDark key={item.uni} {...item} />
+                <InputField key={item.uni} {...item} />
               ))}
             </div>
             <button type="submit" className=' bg-primary self-end rounded-sm p-1 md: w-28'>Submit</button>
