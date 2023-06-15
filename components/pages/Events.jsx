eeb2dac36b639cd859d91bc1a4fbf8cf06945961
Aryan Thakur <aryan197297@gmail.com>
2023-06-15 13:22:50 +0530

    feat: event card done

---
+++ components/pages/Events.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+import { assets } from "../assets";
+import Image from "next/image";
+import { AccessTimeOutlined, LocationOnOutlined } from "@mui/icons-material";
+import Link from "next/link";
+
+const Events = () => {
+  const eventList = [
+    {
+      title: "Sing Along",
+      image: assets.whyChoose,
+      date: "10-Jun-2023",
+      time: "10:00 AM 12:00 PM",
+      location: "Delhi",
+      description:
+        "This is a description of the event, The maximum characters shown in card is 50",
+      organiser: "John Doe",
+    },
+    {
+      title: "Sing Along",
+      image: assets.whyChoose,
+      date: "10-Jun-2023",
+      time: "10:00 AM 12:00 PM",
+      location: "Delhi",
+      description:
+        "This is a description of the event, The maximum characters shown in card is 50",
+      organiser: "John Doe",
+    },
+  ];
+  const EventCard = ({
+    title,
+    image,
+    date,
+    time,
+    location,
+    description,
+    organiser,
+  }) => {
+    return (
+      <div className="text-black border-2 overflow-hidden rounded-md max-w-[290px] border-gray-400 flex flex-col gap-3">
+        <div className="px-2 text-center bg-primary bg-opacity-80 w-fit text-xl mb-[-68px] z-10 text-white relative">
+          <div className="text-sm">{date.split("-")[1]}</div>
+          <div className="text-3xl">{date.split("-")[0]}</div>
+        </div>
+        <Image src={image} className="w-full" />
+        <div className="p-2 gap-2 flex flex-col">
+          <div>
+            <div className="text-slate-500 text-sm">{organiser}</div>
+            <h1 className="font-bold text-xl">{title.toUpperCase()}</h1>
+          </div>
+          <div className="flex text-sm flex-col sm:flex-row gap-1 sm:items-center">
+            <div className="flex gap-1 items-center bg-primary text-white rounded-full px-2 p-[1px]">
+              <LocationOnOutlined />
+              {location}
+            </div>
+            <div className="bg-primary text-white flex gap-1 items-center rounded-full px-2 p-[1px]">
+              <AccessTimeOutlined />
+              {time}
+            </div>
+          </div>
+          <div className="text-slate-400">
+            {description.slice(0, 50) + "..."}
+            <Link href="" className="text-blue-500">
+              Read More
+            </Link>
+          </div>
+          <button className="bg-secondary text-white drop-shadow-md p-1 font-bold text-2xl rounded-sm">
+            Get Started
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Dashboard>
+      <div className="flex gap-2 flex-wrap">
+        {eventList.map((item, i) => (
+          <EventCard
+            title={item.title}
+            image={item.image}
+            date={item.date}
+            time={item.time}
+            location={item.location}
+            description={item.description}
+            organiser={item.organiser}
+          />
+        ))}
+      </div>
+    </Dashboard>
+  );
+};
+
+export default Events;
---
+++ pages/events/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import DashboardDefault from "../../components/pages/DashboardDefault";
+import Events from "../../components/pages/Events";
+
+export default function index() {
+  return (
+    <>
+      <Head>
+        <title>Research - Dashboard</title>
+        <meta charSet="utf-8" />
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/da75fckow/image/upload/v1684919760/sikka-warehouse/logo_e3zr7m.png"
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta name="description" content="research_description" />
+        <meta property="og:title" content="Research - Dashboard" />
+        <meta name="keywords" content="research_keywords" />
+
+        <meta property="og:url" content="reseasrch_url" />
+        {/* <meta name="author" content="Your name here" /> */}
+        <meta
+          property="og:image"
+          itemProp="https://res.cloudinary.com/da75fckow/image/upload/v1684919760/sikka-warehouse/logo_e3zr7m.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Type" content="text/html; charSet=utf-8" />
+        <link rel="canonical" href="https://3plwarehouseservicez.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="research_description" />
+        <meta property="og:site_name" content="Research" />
+      </Head>
+      <Events />
+    </>
+  );
+}
