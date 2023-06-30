85c5f2fd2892e7d7b3b19c01b6a0d16fcf5619a8
iamraselmolla <raselmolla6336@gmail.com>
2023-06-30 11:18:19 +0600

    added spinner at all-users page

--- components/pages/AllUsers.jsx
+++ components/pages/AllUsers.jsx
@@ -3,18 +3,19 @@ import { useContext, useEffect, useState } from "react";
 import Dashboard from "./Dashboard"
 import AuthContext from "../store/AuthContext";
 import Delete from "@mui/icons-material/Delete";
-import BlockIcon  from "@mui/icons-material/Block";
+import BlockIcon from "@mui/icons-material/Block";
 import { fetchAllUsers } from "../services/userServices";
 import { toast } from "react-toastify";
+import { CircularProgress } from "@mui/material";
 const AllUsers = () => {
-  const {token} = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
 
 
   const [allUsers, setAllUser] = useState(null);
   const [loading, setLoading] = useState(false)
   useEffect(() => {
     const fetchData = async () => {
-     
+
       setLoading(true)
 
       try {
@@ -23,7 +24,7 @@ const AllUsers = () => {
         setLoading(false)
       } catch (error) {
         setLoading(false)
-        toast.error(error.response.data)
+        toast.error(error.response.data);
       } finally {
         setLoading(false)
 
@@ -36,39 +37,44 @@ const AllUsers = () => {
 
   return (
     <Dashboard>
-      <div className="w-full">
-        <table className="w-full bg-white border border-black overflow-auto">
-          <thead>
-            <tr className="bg-secondary text-white">
-              <th className="px-4 py-2 text-black border border-black text-center align-middle">SL. No.</th>
-              <th className="px-4 py-2 text-black border border-black text-center align-middle">First Name</th>
-              <th className="px-4 py-2 text-black border border-black text-center align-middle">Last Name</th>
-              <th className="px-4 py-2 text-black border border-black text-center align-middle">Username</th>
-              <th className="px-4 py-2 text-black border border-black text-center align-middle">Role</th>
-              <th className="px-4 py-2 text-black border border-black text-center align-middle">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {allUsers && allUsers.length > 0 && allUsers.map((user, index) => {
-              return (
-                <tr key={index}>
-                  <td className="px-4 py-2 text-black border border-black text-center align-middle">{index + 1}</td>
-                  <td className="px-4 py-2 text-black border border-black text-center align-middle">{user.basicInfo.firstName}</td>
-                  <td className="px-4 py-2 text-black border border-black text-center align-middle">{user.basicInfo.lastName}</td>
-                  <td className="px-4 py-2 text-black border border-black text-center align-middle">{user.basicInfo.username}</td>
-                  <td className="px-4 py-2 text-black border border-black text-center align-middle">{user.role}</td>
-                  <td className="px-4 py-2 text-black border border-black text-center align-middle">
+      {!loading && allUsers &&
+        <div className="w-full">
+          <table className="w-full bg-white border border-black overflow-auto">
+            <thead>
+              <tr className="bg-secondary text-white">
+                <th className="px-4 py-2 text-black border border-black text-center align-middle">SL. No.</th>
+                <th className="px-4 py-2 text-black border border-black text-center align-middle">First Name</th>
+                <th className="px-4 py-2 text-black border border-black text-center align-middle">Last Name</th>
+                <th className="px-4 py-2 text-black border border-black text-center align-middle">Username</th>
+                <th className="px-4 py-2 text-black border border-black text-center align-middle">Role</th>
+                <th className="px-4 py-2 text-black border border-black text-center align-middle">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allUsers && allUsers.length > 0 && allUsers.map((user, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="px-4 py-2 text-black border border-black text-center align-middle">{index + 1}</td>
+                    <td className="px-4 py-2 text-black border border-black text-center align-middle">{user.basicInfo.firstName}</td>
+                    <td className="px-4 py-2 text-black border border-black text-center align-middle">{user.basicInfo.lastName}</td>
+                    <td className="px-4 py-2 text-black border border-black text-center align-middle">{user.username}</td>
+                    <td className="px-4 py-2 text-black border border-black text-center align-middle">{user.role}</td>
+                    <td className="px-4 py-2 text-black border border-black text-center align-middle">
 
-                    <Delete />
-                    <BlockIcon />
-                  </td>
-                </tr>
-              );
-            })}
-            {/* Add more rows as needed */}
-          </tbody>
-        </table>
-      </div>
+                      <Delete />
+                      <BlockIcon />
+                    </td>
+                  </tr>
+                );
+              })}
+              {/* Add more rows as needed */}
+            </tbody>
+          </table>
+        </div>
+      }
+      {loading && <div className="flex justify-center overflow-hidden">
+          <CircularProgress size={50} sx={{ color: 'red' }} />
+        </div>}
 
     </Dashboard >
   );
