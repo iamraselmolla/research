e86ab5f754488612a8e0f09c5dc73185f8996e46
iamraselmolla <raselmolla6336@gmail.com>
2023-06-30 22:43:37 +0600

    added homepage content

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { assets } from '../assets'
 import ResponsiveDrawer from '../UI/ResponsiveDrawer'
 import Footer from '../UI/Footer'
-import { Agriculture, ArticleOutlined, CarRental, Delete, FoodBank, Hardware, HealthAndSafety, Liquor, LocalDining, LocalShipping, MobileFriendly, Refresh, ShoppingCart, Warehouse } from '@mui/icons-material'
+import { Agriculture, ArticleOutlined, CarRental, Delete, FoodBank, Hardware, HealthAndSafety, Liquor, LocalDining, LocalShipping, MobileFriendly, Refresh, ShoppingCart, Warehouse,CheckCircleOutline } from '@mui/icons-material'
 import SplashScreen from '../SplashScreen'
 import Link from 'next/link'
 import { TypeAnimation } from 'react-type-animation'
@@ -21,11 +21,12 @@ const HomePage = () => {
   }, [])
 
   const publishCardData = [
-    { image: assets.icons.signUp, head: "SIGN UP", data: "Create your author account and get exclusive access to Author Dashboard." },
-    { image: assets.icons.contract, head: "CONTRACT", data: "On acceptance, finish up the author-publisher agreement in the dashboard." },
-    { image: assets.icons.marketing, head: "MARKETING", data: "Spreading the word is on us and you d be glad to do it with us." },
-    { image: assets.icons.review, head: "BOOK REVIEW", data: "Upon submitting your book, we will review it within 24 hours (guided publishing) and 120 hours (Partnered Publishing)." },
-    { image: assets.icons.publish, head: "PUBLISH", data: "Time to experience hassle-free publishing with Cleverfox." },
+    { image: assets.icons.signUp, head: "SIGN UP", data: "Create your Student account and get  access to Student Dashboard." },
+    { image: assets.icons.contract, head: "EVENTS", data: "Join in any kind of events and take inspiration for your research work" },
+    { image: assets.icons.contract, head: "VERIFICATION", data: "Upload your student id card and relevant document to verify account" },
+    { image: assets.icons.marketing, head: "UPLOAD", data: "Upload research works to us and get an awesome experience" },
+    { image: assets.icons.review, head: "REVIEW", data: "Wait for review from expert researchers and faculty." },
+    { image: assets.icons.publish, head: "PUBLISH", data: "Publish your research work, take advice and so on" },
   ]
 
   const PublishCard = ({ head, data, image }) => {
@@ -76,21 +77,22 @@ const HomePage = () => {
           <div className='bg-white'>
             <div className='flex flex-col md:flex-row gap-5 bg-white text-black w-[97%] md:w-[80%] m-auto'>
               <div className='flex flex-col justify-center gap-4'>
-                <p className='tracking-widest text-2xl'>Bringing your adult BOOK TO LIFE</p>
-                <p className='font-extrabold tracking-tight text-2xl'>Join the India’s Fastest Growing Book Publishing Platform!</p>
+                <p className='tracking-widest text-2xl'>Dive into a World of <br/>
+                <strong> RESEARCHES AND  EVENTS</strong></p>
+                <p className='font-extrabold tracking-tight text-2xl'>Mark your calendar and attend conferences that inspire and connect you with leading experts in your field. Don't miss to submit your research paper here</p>
                 <button type='button' className='w-48 bg-secondary rounded-md p-2 text-white tracking-widest drop-shadow-md'>GET STARTED</button>
               </div>
-              <Image src={assets.books} className='md:max-w-[70%] w-full'></Image>
+              <Image src={assets.books} className='md:max-w-[50%] w-full'></Image>
             </div>
           </div>
 
           <div className="bg-primary text-white">
             <div className="flex flex-col w-[97%] md:w-[80%] m-auto">
               <div className="flex flex-col md:flex-row  justify-between gap-10 my-10">
-                <div className='text-center'><h2 className='font-extrabold text-4xl'>21000+</h2><p className='text-md'>Community Members</p></div>
-                <div className='text-center'><h2 className='font-extrabold text-4xl'>2000+</h2><p className='text-md'>Books</p></div>
-                <div className='text-center'><h2 className='font-extrabold text-4xl'>150+</h2><p className='text-md'>Countries Reach</p></div>
-                <div className='text-center'><h2 className='font-extrabold text-4xl'>5</h2><p className='text-md'>Years Experience</p></div>
+                <div className='text-center'><h2 className='font-extrabold text-4xl'>21000+</h2><p className='text-md'>Research papers submitted</p></div>
+                <div className='text-center'><h2 className='font-extrabold text-4xl'>2000+</h2><p className='text-md'>Events</p></div>
+                <div className='text-center'><h2 className='font-extrabold text-4xl'>150+</h2><p className='text-md'>Researcher</p></div>
+                <div className='text-center'><h2 className='font-extrabold text-4xl'>5</h2><p className='text-md'>Faculty</p></div>
               </div>
               <div className="flex flex-col justify-center text-xl my-10 text-center">
                 <h2>SIGN UP TO START PUBLISHING NOW</h2>
@@ -124,8 +126,8 @@ const HomePage = () => {
 
           <div className="bg-orange-500 text-white text-center">
             <div className="w-[90%] md:w-[80%] m-auto py-10">
-              <h2 className='tracking-wider text-3xl   font-bold'>PUBLISH WITH US</h2>
-              <p className="my-5">Become an Author in 5 Simple Steps</p>
+              <h2 className='tracking-wider text-3xl   font-bold'>PUBLISH RESEARCH PAPER TO US</h2>
+              <p className="my-5">Become a research student in some simple steps</p>
               <div className='grid grid-cols-1 sm:grid-cols-3  gap-8 sm:place-items-start place-items-center'>
                 {publishCardData.map((item, i) => (
                   <PublishCard head={item.head} data={item.data} image={item.image} />
@@ -135,18 +137,20 @@ const HomePage = () => {
           </div>
 
           <div className='bg-white flex flex-col gap-5 py-20 '>
-            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>AUTHOR BENEFITS</h2>
+            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>Faculty Benefits: Empowering Academic Excellence</h2>
             <p className='text-black text-center md:w-[50%] self-center'>Irrespective of whether you are a first-time publisher, or an experienced author, here is why Clever Fox Publishing is the perfect fit for you. </p>
             <div className='flex md:flex-row flex-col bg-white text-black w-[97%] md:w-[80%] m-auto'>
               <Image src={assets.benefits} className='rounded-xl w-full md:max-w-[50%]'></Image>
               <div className='flex flex-col justify-center p-10 md:p-0 md:items-center md:w-[50%]'>
-                <ul className='flex flex-col gap-5 list-disc'>
-                  <li>Highest Royalty rates</li>
-                  <li>Live Royalty Track Dashboard</li>
-                  <li>Expeditious publishing process </li>
-                  <li>Expanded global distribution </li>
-                  <li>Physical store distribution Support </li>
-                  <li>Hand picked publishing team</li>
+                <ul className='flex flex-col gap-5 list-none list-disc'>
+                  <li><CheckCircleOutline/> Amplify Your Research</li>
+                  <li><CheckCircleOutline/> Foster Collaboration</li>
+                  <li><CheckCircleOutline/> Recognition and Publication </li>
+                  <li><CheckCircleOutline/> Professional Development </li>
+                  <li><CheckCircleOutline/> Networking and Events</li>
+                  <li><CheckCircleOutline/> Funding Opportunities</li>
+                  <li><CheckCircleOutline/> Personalized Support</li>
+                  <li><CheckCircleOutline/> Personalized Support</li>
                 </ul>
 
               </div>
@@ -206,9 +210,9 @@ const HomePage = () => {
 
 
           <div className='bg-pink-500 flex flex-col gap-5 py-10 text-white '>
-            <h2 className='tracking-wider text-3xl  font-bold text-center'>PRODUCTS</h2>
+            <h2 className='tracking-wider text-3xl  font-bold text-center'>SOME OF OUR RECENT ACTIVITIES</h2>
             <p className=' text-center md:w-[50%] self-center'>
-              Read the collections of our top trending <b>Self Publishing</b> and <b>Partnered Publishing books</b>
+              Read some of our  reviewed <b>Research</b> and <b>arranged events</b>
             </p>
             <div className='w-[96%] md:w-[80%] m-auto overflow-x-hidden flex flex-col items-center gap-5'>
               <div className='flex w-[96%] m-auto overflow-x-hidden items-center'>
@@ -229,17 +233,17 @@ const HomePage = () => {
           </div>
 
           <div className='bg-white flex flex-col gap-5 py-20'>
-            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>AUTHOR DASHBOARD</h2>
+            <h2 className='tracking-wider text-3xl text-ter font-bold text-center'>ADMIN DASHBOARD</h2>
             <p className='text-black text-center md:w-[50%] self-center'>
               Get real-time numbers on your sales and track the order status.
             </p>
             <div className='flex md:flex-row flex-col-reverse bg-white text-black w-[96%] md:w-[80%] m-auto'>
               <div className='flex flex-col md:items-center md:justify-center m-10 md:m-0 min-w-[50%]'>
                 <ul className='flex flex-col gap-5'>
-                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 border-blue-700 p-2 w-56'>Sales Tracker {">>"}</button>
-                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>Instant Order {">>"}</button>
-                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>Advert Dashboard {">>"}</button>
-                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>Postsales Analysis {">>"}</button>
+                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 border-blue-700 p-2 w-56'>All Users Info {">>"}</button>
+                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>All Event Management {">>"}</button>
+                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>Approve Faculty Account {">>"}</button>
+                  <button type="button" className='bg-blue-200 rounded-tr-md rounded-tl-md border-b-2 text-blue-700 p-2 w-56'>Application Status {">>"}</button>
                 </ul>
               </div>
               <Image src={assets.dashboard} className='w-full md:max-w-[50%]' />
