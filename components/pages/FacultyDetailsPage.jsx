c5d708a21085d5662e61c5eb4e7b184ec47f18ec
iamraselmolla <raselmolla6336@gmail.com>
2023-07-10 17:57:05 +0600

    factored faculty details page

--- components/pages/FacultyDetailsPage.jsx
+++ components/pages/FacultyDetailsPage.jsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router"
 import Spinner from "../../components/UI/Spinner"
 import { useEffect, useState } from "react";
 import ResponsiveDrawer from "../../components/UI/ResponsiveDrawer";
-import { Cake, ConnectWithoutContact, Email, EscalatorWarning, InfoOutlined, People, Person, Phone, School, TempleHindu, Work } from "@mui/icons-material";
+import {  CakeOutlined, Email, EscalatorWarning, Handshake, InfoOutlined, People, Person, Phone, School, TempleHindu, Work } from "@mui/icons-material";
 import SplashScreen from "../../components/SplashScreen";
 import Footer from "../../components/UI/Footer";
 import { Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
@@ -60,7 +60,10 @@ const FacultyDetailsPage = () => {
             {loading ? <SplashScreen /> :
                 <>
                     <ResponsiveDrawer />
-                    <TopCard title={faculty?.basicInfo.firstName + ' ' + faculty?.basicInfo.lastName} />
+                    {faculty &&
+                    
+                    <>
+                        <TopCard title={faculty?.basicInfo.firstName + ' ' + faculty?.basicInfo.lastName} />
                     <div className='py-20 bg-white text-black '>
                         <Container>
                             {loading && <div className='py-20 '>
@@ -83,36 +86,35 @@ const FacultyDetailsPage = () => {
                                                 </div> */}
                                         </div>
                                         <div className='flex gap-4'><Person sx={{ color: '#f72151' }} fontSize='medium' /><h3>{calculateAge(faculty.basicInfo.dob)} years old </h3></div>
-                                        <div className='flex gap-4'><EscalatorWarning sx={{ color: '#f72151' }} fontSize='medium' /><h3>{faculty.maritalStatus}</h3></div>
-                                        <div className='flex gap-4'><TempleHindu sx={{ color: '#f72151' }} fontSize='medium' /><h3>{`${faculty.religion} / ${faculty.caste} / ${faculty.subCaste}`}</h3></div>
-                                        <div className='flex gap-4'><Work sx={{ color: '#f72151' }} fontSize='medium' /><h3>{faculty.occupation} / {faculty.employedIn}</h3></div>
+                                        <div className='flex gap-4'><CakeOutlined sx={{ color: '#f72151' }} fontSize='medium' /><h3>{new Date(faculty.basicInfo.dob).toLocaleDateString()}</h3></div>
+                                        {/* <div className='flex gap-4'><TempleHindu sx={{ color: '#f72151' }} fontSize='medium' /><h3>{`${faculty.religion} / ${faculty.caste} / ${faculty.subCaste}`}</h3></div> */}
+                                        {/* <div className='flex gap-4'><Work sx={{ color: '#f72151' }} fontSize='medium' /><h3>{faculty.occupation} / {faculty.employedIn}</h3></div> */}
                                         {/* <div className='flex gap-4'><LocationOn sx={{color:'#f72151'}} fontSize='medium'/><h3>{faculty.state} / {faculty.city}</h3></div> */}
-                                        <div className='flex gap-4'><Person sx={{ color: '#f72151' }} fontSize='medium' /><h3>Joined as Faculty : {new Date(faculty.createdAt).toLocaleString()}</h3></div>
+                                        <div className='flex gap-4'><Handshake sx={{ color: '#f72151' }} fontSize='medium' /><h3>Joined as Faculty : {new Date(faculty.createdAt).toLocaleString()}</h3></div>
                                     </div>
 
                                     {/* Basic Details */}
-                                    <CardWrapper title='BASIC DETAILS'>
+                                    {/* <CardWrapper title='BASIC DETAILS'>
                                         <h3>Gender : <span className='font-semibold'>{faculty.basicInfo.gender}</span></h3>
                                         <h3>DOB : <span className='font-semibold'>{new Date(faculty.basicInfo.dob).toLocaleDateString()}</span></h3>
                                         <h3>Height : <span className='font-semibold'></span></h3>
                                         <h3>Mother Tongue : <span className='font-semibold'></span></h3>
-                                        {/* {
+                                        {
                                                 faculty.maritalStatus !== 'Never Married' ? (
                                                     <h3 >No of Children : <span className='font-semibold'>{
                                                         faculty.noOfChildren === '0' || faculty.noOfChildren === "" ? 'None' : faculty.noOfChildren
                                                     }</span></h3>
                                                 ) : null
-                                            }  */}
+                                            }  
                                         <h3>Weight : <span className='font-semibold'> Kg</span></h3>
                                         <h3>Body Type :  <span className='font-semibold'></span></h3>
                                         <h3>Complexion : <span className='font-semibold'></span></h3>
                                         <h3>Physical Status : <span className='font-semibold'></span></h3>
                                         <h3>Blood Group : <span className='font-semibold'></span></h3>
                                         <h3>Eating Habits : <span className='font-semibold'></span></h3>
-                                    </CardWrapper>
-                                    Astrological Details
+                                    </CardWrapper> */}
 
-                                    <CardWrapper title='ASTROLOGICAL DETAILS'>
+                                    {/* <CardWrapper title='ASTROLOGICAL DETAILS'>
                                         <h3>Birth Hour : <span className='font-semibold'></span></h3>
                                         <h3>Birth Minute : <span className='font-semibold'></span></h3>
                                         <h3>Birth Place : <span className='font-semibold'></span></h3>
@@ -120,13 +122,13 @@ const FacultyDetailsPage = () => {
                                         <h3>Star : <span className='font-semibold'></span></h3>
                                         <h3>Raasi / Moon sign : <span className='font-semibold'></span></h3>
                                         <h3>Manglik : <span className='font-semibold'></span></h3>
-                                    </CardWrapper>
+                                    </CardWrapper> */}
                                     
 
                                    
                                 </div>
                                 <div>
-                                    <h2 className="col-span-1 sm:col-span-2 text-ter text-2xl">Professional Details</h2>
+                                    <h2 className="col-span-1 mt-5 sm:col-span-2 text-ter text-2xl">Professional Details</h2>
                                         <h3 className="col-span-1 sm:col-span-2  text-xl ">Education : </h3> 
                                             <div className="relative overflow-x-auto">
                                                 <table className="w-full text-sm text-center  text-gray-500 dark:text-gray-400">
@@ -173,6 +175,8 @@ const FacultyDetailsPage = () => {
                         <Footer />
 
                     </div>
+                    </>
+                    }
                 </>
             }
         </div>
