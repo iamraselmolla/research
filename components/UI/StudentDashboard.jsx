9acbf8394f869eb58a8f8d2c4e6c3214aa90b849
iamraselmolla <raselmolla6336@gmail.com>
2023-07-20 00:38:14 +0600

    added student chart

--- components/UI/StudentDashboard.jsx
+++ components/UI/StudentDashboard.jsx
@@ -1,83 +1,96 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const StudentDashboard = () => {
     const { user, research } = useSelector(state => state.user);
+
     const pending = research?.filter(singleResearch => singleResearch.status === 'pending').length;
     const approved = research?.filter(singleResearch => singleResearch.status === 'approved').length;
     const rejected = research?.filter(singleResearch => singleResearch.status === 'rejected').length;
-    const data = [
-        { name: "Group A", value: pending },
-        { name: "Group B", value: approved },
-        { name: "Group C", value: rejected }
+    const assigned = research?.filter(singleResearch => singleResearch.assigned).length;
+    const total = research?.length
+
+    const data1 = [
+        { name: 'A', value: pending },
+        { name: 'B', value: approved },
+        { name: 'C', value: rejected }
     ];
 
-    const COLORS = ["#3B82F6", "#22C55E", "#EF4444"];
+    const data2 = [
+        { name: 'X', value: assigned },
+        { name: 'Y', value: (total - assigned) }
+    ];
 
-    const RADIAN = Math.PI / 180;
-    const renderCustomizedLabel = ({
-        cx,
-        cy,
-        midAngle,
-        innerRadius,
-        outerRadius,
-        percent,
-        index
-    }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    const COLORS = ['#3B82F6', '#22C55E', '#EF4444'];
+    const COLORS2 = ['#8884d8', '#82ca9d'];
 
-        return (
-            <text
-                x={x}
-                y={y}
-                fill="white"
-                textAnchor={x > cx ? "start" : "end"}
-                dominantBaseline="central"
-            >
-                {`${(percent * 100).toFixed(0)}%`}
-            </text>
-        );
-    };
     return (
-
         <>
-            <div className="mt-3">
-                <PieChart width={400} height={400}>
-                    <Pie
-                        data={data}
-                        cx={200}
-                        cy={200}
-                        labelLine={false}
-                        label={renderCustomizedLabel}
-                        outerRadius={80}
-                        fill="#8884d8"
-                        dataKey="value"
-                    >
-                        {data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                        ))}
-                    </Pie>
-                </PieChart>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
 
-                <div className="legend_of_chart flex flex-col gap-3 text-black font-extrabold">
-                    <div className='flex gap-3 items-center'>
-                        <div className="w-5 h-5 rounded-full bg-blue-500"></div> <div>
-                            Pending - {pending}
-                        </div>
-                    </div>
-                    <div className='flex gap-3 items-center'>
-                        <div className="w-5 h-5 rounded-full bg-green-500"></div> <div>
-                            Approved - {approved}
-                        </div>
-                    </div>
-                    <div className='flex gap-3 items-center'>
-                        <div className="w-5 h-5 rounded-full bg-red-500"></div> <div>
-                            Rejected - {rejected}
-                        </div>
-                    </div>
+                    <ResponsiveContainer>
+                        <PieChart width={400} height={400}>
+                            <Pie
+                                data={data1}
+                                cx={200}
+                                cy={200}
+                                labelLine={false}
+                                label={({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+                                    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+                                    const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
+                                    const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
+
+                                    return (
+                                        <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="central">
+                                            {`${(percent * 100).toFixed(0)}%`}
+                                        </text>
+                                    );
+                                }}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {data1.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
+                        </PieChart>
+
+                    </ResponsiveContainer>
+                    
+                </div>
+                <div>
+                    <ResponsiveContainer width="100%" height={500}>
+                        <PieChart>
+                            <Pie
+                                data={data2}
+                                cx={200}
+                                cy={200}
+                                labelLine={false}
+                                label={({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+                                    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+                                    const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
+                                    const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
+
+                                    return (
+                                        <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="central">
+                                            {`${(percent * 100).toFixed(0)}%`}
+                                        </text>
+                                    );
+                                }}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {data2.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS2[index % COLORS.length]} />
+                                ))}
+                                
+                            </Pie>
+                        </PieChart>
+                    </ResponsiveContainer>
                 </div>
             </div>
         </>
